0547817cf6836e83edbfa3797a5049d96ad89d7f
rengj <ren.gj@hotmail.com>
2016-04-29 01:18:25 -0400

    20160429 predict

--- Ru250/src/main/java/com/ru/controller/PredictController.java
+++ Ru250/src/main/java/com/ru/controller/PredictController.java
@@ -13,29 +13,33 @@ import com.ru.service.StockService;
 
 @Controller
 public class PredictController {
+	private StockService stockService;
+
 	@Autowired(required=true)
 	@Qualifier(value="stockService")
-	private StockService stockService;
-	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public StockService getStockService() {
+		return stockService;
+	}
+
+	@RequestMapping(value = "/1", method = RequestMethod.POST)
     public List<Double> predictHStockANN(@PathVariable("stock") String stock, @PathVariable("dayToPredict") int dayToPredict) {
 		List<Double> list = stockService.predictValueByHStockANN(stock, dayToPredict);
         return list;
     }
 	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@RequestMapping(value = "/2", method = RequestMethod.POST)
     public List<Double> predictStockANN(@PathVariable("stock") String stock, @PathVariable("dayToPredict") int dayToPredict) {
 		List<Double> list = stockService.predictValueByStockANN(stock, dayToPredict);
         return list;
     }
 	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@RequestMapping(value = "/3", method = RequestMethod.POST)
     public double predictStockBayesian(@PathVariable("stock") String stock) {
 		double result = stockService.predictValueByStockBayesian(stock);
         return result;
     }
 	
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@RequestMapping(value = "/4", method = RequestMethod.POST)
     public double predictHStockBayesian(@PathVariable("stock") String stock) {
 		double result = stockService.predictValueByHStockBayesian(stock);
         return result;
